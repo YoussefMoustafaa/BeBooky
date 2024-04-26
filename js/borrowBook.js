9ae485ec4d55f7e4b4ea5972abbb1640e1048b50
MazenEhab01 <mazen.ehab3000@gmail.com>
2024-04-26 16:35:44 +0300

    borrow book .js done but we need to remove some things from the local storage because it is fillled and  all the books are borrowed

--- js/borrowBook.js
+++ js/borrowBook.js
@@ -8,39 +8,43 @@ if(selectedBook.isBorrowed==true){
 }
 else{
     selectedBook.isBorrowed = true
-    console.log(activeUser);
-    console.log(selectedBook);
-    activeUser.borrowedBooks.push(selectedBook)
+    // console.log(activeUser);
+    // console.log(selectedBook);
+    // activeUser.borrowedBooks.push(selectedBook)
     let users = JSON.parse(localStorage.getItem('users'))
     console.log(users);
 
     
-    var users = users.filter((user) => {
-        console.log(user.userID);
-        console.log(activeUser.userID);
-        return user.userID != activeUser.userID ;
+         users = users.filter((user) => {
+        // console.log(user.username)
+        // console.log(activeUser.username);
+        return user.username != activeUser.username 
     })
     console.log(users);
-    console.log(users[i]);
-    users[i].borrowedBooks.push(selectedBook)
-    console.log(activeUser);
+//     console.log(i);
+//     console.log(users[i]);
+//     users[i].borrowedBooks.push(selectedBook)
+//     console.log(activeUser);
+    activeUser.borrowedBooks.push(selectedBook);
+    users.push(activeUser);
+    
 
 
-    localStorage.setItem('user', JSON.stringify(users))
+     localStorage.setItem('user', JSON.stringify(users))
 
-    localStorage.setItem('activeUser', JSON.stringify(activeUser))
+   localStorage.setItem('activeUser', JSON.stringify(activeUser))
 
     let AllBooks = JSON.parse(localStorage.getItem("books"))
     console.log(AllBooks);
 
-    let j = AllBooks.indexOf((book) => {
-        return book.id == selectedBook.id;
+    AllBooks = AllBooks.filter((book) => {
+        return book.id != selectedBook.id;
     })
 
-    AllBooks[j].isBorrowed = true
-
+    AllBooks.push(selectedBook);
     localStorage.setItem('books', JSON.stringify(AllBooks))
 
     console.log(AllBooks);
     alert("Book borrowed successfully");
+
 }
