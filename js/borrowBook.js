68349f4fd377905e77dc373942d335ee1ca7d4d1
MazenEhab01 <mazen.ehab3000@gmail.com>
2024-04-26 16:00:59 +0300

    borrow book and add book
    check login.js
    changed borrowbook.js and bookdetails
    now when admin is logged
    borrow doesnt appear

--- js/borrowBook.js
+++ js/borrowBook.js
@@ -2,6 +2,7 @@ let activeUser = JSON.parse(localStorage.getItem('activeUser'))
 
 var selectedBook = JSON.parse(sessionStorage.getItem('selectedBook'))
 
+console.log(selectedBook)
 if(selectedBook.isBorrowed==true){
     alert("Book is already borrowed");
 }
@@ -11,18 +12,26 @@ else{
     console.log(selectedBook);
     activeUser.borrowedBooks.push(selectedBook)
     let users = JSON.parse(localStorage.getItem('users'))
-    
-    let i = users.indexOf((user) => {
-        return user.userID == activeUser.userID
-    })
+    console.log(users);
 
+    
+    var users = users.filter((user) => {
+        console.log(user.userID);
+        console.log(activeUser.userID);
+        return user.userID != activeUser.userID ;
+    })
+    console.log(users);
+    console.log(users[i]);
     users[i].borrowedBooks.push(selectedBook)
+    console.log(activeUser);
+
 
     localStorage.setItem('user', JSON.stringify(users))
 
     localStorage.setItem('activeUser', JSON.stringify(activeUser))
 
     let AllBooks = JSON.parse(localStorage.getItem("books"))
+    console.log(AllBooks);
 
     let j = AllBooks.indexOf((book) => {
         return book.id == selectedBook.id;
@@ -32,5 +41,6 @@ else{
 
     localStorage.setItem('books', JSON.stringify(AllBooks))
 
+    console.log(AllBooks);
     alert("Book borrowed successfully");
 }
